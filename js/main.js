03a81b8d6b0335354fe960f6cdaad2f7cf7fa8f2
mateusz0dziubinski <mateusz0dziubinski@o2.pl>
2019-09-09 21:44:54 +0200

    Zadanie: zrobione

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-let liczbaPierwsza = 5;
-let liczbaDruga = 10;
+var number1 = 10;
+var number2 = 30;
 
-let suma = liczbaPierwsza + liczbaDruga;
+function sum(){
+    return number1 + number2;
+}
 
-console.log(suma);
+console.log(sum());
